c2144478d685f72f780407bcc75ad765af59dea2
Artiom <sinigur2006@gmail.com>
2026-01-27 23:34:21 -0500

    Setup websocket

--- index-ws.js
+++ index-ws.js
@@ -10,3 +10,29 @@ server.on('request', app);
 server.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
+
+// Define WebSocket behavior
+const WebSocket = require('ws').Server;
+const wss = new WebSocket({ server: server });
+
+wss.on('connection', (ws) => {
+    const numClients = wss.clients.size;
+    wss.broadcast(`Current visitors: ${numClients}`);
+    console.log('New client connected: ', numClients);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome to the WebSocket server!');
+    }
+
+    ws.on('close', () => {
+        const numClients = wss.clients.size;
+        wss.broadcast(`Current visitors: ${numClients}`);
+        console.log('Client disconnected: ', numClients);
+    });
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+};
